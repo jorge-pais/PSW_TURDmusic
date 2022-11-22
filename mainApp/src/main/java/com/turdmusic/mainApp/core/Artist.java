4583e673445dcf90ea0f472745c3bef5d3d5d30c
Jorge Pais <jorge27pais@gmail.com>
2022-11-22 16:43:42 +0000

    moved old sources and renamed main application project

---
+++ mainApp/src/main/java/com/turdmusic/mainApp/core/Artist.java
@@ -0,0 +1,51 @@
+package com.turdmusic.mainApp.core;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import java.util.ArrayList;
+
+// Maybe change this to a UUID object identifier
+@JsonIdentityInfo(
+        generator= ObjectIdGenerators.IntSequenceGenerator.class,
+        property="@json_id")
+public class Artist {
+
+    public int id;
+    //private Image picture;
+    private String name;
+    private ArrayList<Music> songs;
+    private ArrayList<Album> albums;
+
+    public Artist(){
+        super();
+    }
+
+    public Artist(String name, int id){
+        this.name = name;
+        this.id = id;
+
+        songs = new ArrayList<>();
+        albums = new ArrayList<>();
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void addSong(Music song){
+        songs.add(song);
+    }
+    public void removeSong(Music song){songs.remove(song);}
+
+    public ArrayList<Album> getAlbums(){
+        return albums;
+    }
+    public ArrayList<Music> getSongs(){return songs;}
+
+    public void addAlbum(Album album){
+        albums.add(album);
+    }
+    public void removeAlbum(Album album){ albums.remove(album); }
+
+}
